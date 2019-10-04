2b2d6712d21c778940f64bd4f0e43d3e7357d5f8
Morgan Holmes <morgan.holmes@live.co.uk>
2019-10-04 11:57:36 +0100

    Wheel and Tyre Class Updated

--- JavaCarClass/Tyre.java
+++ JavaCarClass/Tyre.java
@@ -1,33 +1,29 @@
 
-/**
- * Write a description of class Tyre here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Tyre
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private String make;
+    private String model;
 
     /**
      * Constructor for objects of class Tyre
      */
-    public Tyre()
+    public Tyre(String make, String model)
     {
-        // initialise instance variables
-        x = 0;
+        this.make = make;
+        this.model = model;
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public String getMake(){
+        return make;
     }
+    
+    public String getModel(){
+        return model;
+    }
+    
+    public String getTyreInfo(){
+        return make + " " + model;
+    }
+    
 }
--- JavaCarClass/Wheel.java
+++ JavaCarClass/Wheel.java
@@ -2,26 +2,72 @@
 public class Wheel
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private double width;
+    private int offset;
+    private int size;
+    private String make;
+    private String model;
+    private Tyre tyre;
 
     /**
      * Constructor for objects of class Wheel
      */
     public Wheel()
     {
-        // initialise instance variables
-        x = 0;
+        model = "Not Set";
+        make = "Not Set";
+        width = 0.0;
+        offset = 0;
+        size = 0;
+    }
+    
+    public Wheel(String make, String model, double width, int offset, int size, String tyreMake, String tyreModel){
+        this.make = make;
+        this.model = model;
+        this.width = width;
+        this.offset = offset;
+        this.size = size;
+        this.tyre = new Tyre(tyreMake, tyreModel);
+    }
+    
+    public Wheel(String make, String model, double width, int offset, int size, Tyre tyre){
+        this.make = make;
+        this.model = model;
+        this.width = width;
+        this.offset = offset;
+        this.size = size;
+        this.tyre = tyre;
+    }
+    
+    public Wheel(String make, String model, double width, int offset, int size){
+        this.make = make;
+        this.model = model;
+        this.width = width;
+        this.offset = offset;
+        this.size = size;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public String getMake(){
+        return make;
+    }
+    
+    public String getModel(){
+        return model;
+    }
+    
+    public double getWidth(){
+        return width;
+    }
+    
+    public int getSize(){
+        return size;
+    }
+    
+    public int getOffset(){
+        return offset;
+    }
+    
+    public String wheelInfo(){
+        return make + " " + model + " Size: " + size + " Width: " + width + " Offset: " + offset; 
     }
 }
